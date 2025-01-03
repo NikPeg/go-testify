3c0bd4830e4ad6e6fb6956715557776e3dda9e0e
NikPeg <peganov.nik@gmail.com>
2025-01-03 14:31:27 +0300

    format

--- main_test.go
+++ main_test.go
@@ -1,59 +1,58 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "strconv"
-    "strings"
-    "testing"
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
 )
 
-
 func TestCorrectRequest(t *testing.T) {
-    totalCount := 4
-    req := httptest.NewRequest("GET", "/cafe?city=moscow&count=" + strconv.Itoa(totalCount), nil)
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(totalCount), nil)
 
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-    require.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 
-    body := responseRecorder.Body.String()
-    list := strings.Split(body, ",")
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
 
-    require.Equal(t, len(list), totalCount)
+	require.Equal(t, len(list), totalCount)
 }
 
 func TestWrongCityValue(t *testing.T) {
-    wrongCity := "karaganda"
-    req := httptest.NewRequest("GET", "/cafe?city=" + wrongCity + "&count=1", nil)
+	wrongCity := "karaganda"
+	req := httptest.NewRequest("GET", "/cafe?city="+wrongCity+"&count=1", nil)
 
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-    require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 
-    body := responseRecorder.Body.String()
+	body := responseRecorder.Body.String()
 
-    require.NotNil(t, body)
-    require.Equal(t, body, "wrong city value")
+	require.NotNil(t, body)
+	require.Equal(t, body, "wrong city value")
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := httptest.NewRequest("GET", "/cafe?city=moscow&count=" + strconv.Itoa(totalCount + 1), nil)
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(totalCount+1), nil)
 
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-    require.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 
-    body := responseRecorder.Body.String()
-    list := strings.Split(body, ",")
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
 
-    require.Equal(t, len(list), totalCount)
+	require.Equal(t, len(list), totalCount)
 }
